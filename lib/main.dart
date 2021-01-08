734f08ccb0c46e9f2cb6e311a4c2b7f82ba071cd
aymanehachcham <aymanehachchaming@gmail.com>
2021-01-08 17:00:12 +0100

    Onboarding page added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:jood/home.dart';
+import 'Onboardign.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(),
+      home: OnBoardingPage(),
     );
   }
 }
